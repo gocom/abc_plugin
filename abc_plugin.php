229dec72adaa35676dc0e676c281b7ebd82005a7
Jukka Svahn <web@rahinaa.biz>
2013-08-09 16:05:47 +0300

    Indent with spaces.

--- abc_plugin.php
+++ abc_plugin.php
@@ -12,50 +12,50 @@
 
 class abc_example
 {
-	/**
-	 * Constructor.
-	 *
-	 * Registers plugin-lifecycle handlers.
-	 */
+    /**
+     * Constructor.
+     *
+     * Registers plugin-lifecycle handlers.
+     */
 
-	public function __construct()
-	{
-		register_callback(array($this, 'install'), 'plugin_lifecycle.abc_example', 'installed');
-		register_callback(array($this, 'uninstall'), 'plugin_lifecycle.abc_example', 'deleted');
-		register_callback(array($this, 'greeting'), 'list', '', true);
-	}
+    public function __construct()
+    {
+        register_callback(array($this, 'install'), 'plugin_lifecycle.abc_example', 'installed');
+        register_callback(array($this, 'uninstall'), 'plugin_lifecycle.abc_example', 'deleted');
+        register_callback(array($this, 'greeting'), 'list', '', true);
+    }
 
-	/**
-	 * Installer.
-	 *
-	 * Run when the plugin is installed or updated.
-	 */
+    /**
+     * Installer.
+     *
+     * Run when the plugin is installed or updated.
+     */
 
-	public function install()
-	{
-	}
+    public function install()
+    {
+    }
 
-	/**
-	 * Uninstaller.
-	 *
-	 * Run when the plugin is deleted.
-	 */
+    /**
+     * Uninstaller.
+     *
+     * Run when the plugin is deleted.
+     */
 
-	public function uninstall()
-	{
-	}
+    public function uninstall()
+    {
+    }
 
-	/**
-	 * Adds the translation string to admin-side Articles panel.
-	 *
-	 * @see gTxt()
-	 * @see graf()
-	 */
+    /**
+     * Adds the translation string to admin-side Articles panel.
+     *
+     * @see gTxt()
+     * @see graf()
+     */
 
-	public function greeting()
-	{
-		echo graf(gTxt('abc_plugin_greeting'), ' class="alert-block information"');
-	}
+    public function greeting()
+    {
+        echo graf(gTxt('abc_plugin_greeting'), ' class="alert-block information"');
+    }
 }
 
 /**
