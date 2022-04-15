9f36b7f9f79e5569f66d4afbbf2e7844856b8531
Jukka Svahn <void@rah.pw>
2022-04-15 22:35:47 +0300

    Update dependencies and template

---
+++ src/Abc/Example.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This is an example plugin for Textpattern CMS.
+ *
+ * @link https://textpattern.com/
+ */
+
+/**
+ * The plugin class.
+ */
+class Abc_Example
+{
+    /**
+     * Constructor.
+     *
+     * Registers plugin-lifecycle handlers.
+     */
+    public function __construct()
+    {
+        register_callback(array($this, 'install'), 'plugin_lifecycle.abc_example', 'installed');
+        register_callback(array($this, 'uninstall'), 'plugin_lifecycle.abc_example', 'deleted');
+        register_callback(array($this, 'greeting'), 'list', '', true);
+    }
+
+    /**
+     * Installer.
+     *
+     * Run when the plugin is installed or updated.
+     */
+    public function install()
+    {
+    }
+
+    /**
+     * Uninstaller.
+     *
+     * Run when the plugin is deleted.
+     */
+    public function uninstall()
+    {
+    }
+
+    /**
+     * Adds the translation string to admin-side Articles panel.
+     *
+     * @see \gTxt()
+     * @see \graf()
+     */
+    public function greeting()
+    {
+        echo graf(gTxt('abc_plugin_greeting'), ' class="alert-block information"');
+    }
+}
--- src/Index.php
+++ src/Index.php
@@ -7,53 +7,6 @@
  */
 
 /**
- * The plugin class.
- */
-class Abc_Example
-{
-    /**
-     * Constructor.
-     *
-     * Registers plugin-lifecycle handlers.
-     */
-    public function __construct()
-    {
-        register_callback(array($this, 'install'), 'plugin_lifecycle.abc_example', 'installed');
-        register_callback(array($this, 'uninstall'), 'plugin_lifecycle.abc_example', 'deleted');
-        register_callback(array($this, 'greeting'), 'list', '', true);
-    }
-
-    /**
-     * Installer.
-     *
-     * Run when the plugin is installed or updated.
-     */
-    public function install()
-    {
-    }
-
-    /**
-     * Uninstaller.
-     *
-     * Run when the plugin is deleted.
-     */
-    public function uninstall()
-    {
-    }
-
-    /**
-     * Adds the translation string to admin-side Articles panel.
-     *
-     * @see \gTxt()
-     * @see \graf()
-     */
-    public function greeting()
-    {
-        echo graf(gTxt('abc_plugin_greeting'), ' class="alert-block information"');
-    }
-}
-
-/**
  * Initializes the plugin.
  */
 new Abc_Example();
