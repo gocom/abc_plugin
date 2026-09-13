d1973d8d9a9126d9746158aecbe2beffecfdfdf3
Jukka Svahn <void@rah.pw>
2026-09-13 17:46:30 +0300

    Adobt brain/monkey to avoid manual doubles

--- test/Unit/ExampleTest.php
+++ test/Unit/ExampleTest.php
@@ -11,29 +11,58 @@ declare(strict_types=1);
 namespace Abc\Plugin\Test\Unit;
 
 use PHPUnit\Framework\TestCase;
-use function Patchwork\{redefine};
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Brain\Monkey\Functions;
+use Brain\Monkey;
 
 final class ExampleTest extends TestCase
 {
+    use MockeryPHPUnitIntegration;
+
     private \Abc_Example $subject;
 
     protected function setUp(): void
     {
+        Functions\stubs(['register_callback']);
+
         $this->subject = new \Abc_Example();
     }
 
-    public function testLifecycleCallbacks(): void
+    public function testConstruct(): void
     {
-        redefine('\register_callback', fn(array $callback) => $this->assertIsArray($callback));
+        Functions\stubs([
+            'register_callback' => fn(array $callback) => $this->assertIsArray($callback),
+        ]);
 
         new \Abc_Example();
     }
 
+    public function testInstall(): void
+    {
+        $this->assertTrue(true);
+
+        $this->subject->install();
+    }
+
+    public function testUninstall(): void
+    {
+        $this->assertTrue(true);
+
+        $this->subject->uninstall();
+    }
+
     public function testGreeting(): void
     {
-        redefine('\gTxt', fn(string $input) => $this->assertSame('abc_plugin_greeting', $input));
-        redefine('\graf', fn() => '');
+        Functions\stubs([
+            'gTxt' => fn (string $input) => $this->assertSame('abc_plugin_greeting', $input),
+            'graf' => '',
+        ]);
 
         $this->subject->greeting();
+    }
+
+    protected function tearDown(): void
+    {
+        Monkey\tearDown();
     }
 }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,8 +4,7 @@
 \ini_set('memory_limit', '512M');
 \error_reporting(E_ALL);
 
-require './vendor/autoload.php';
 require './vendor/antecedent/patchwork/Patchwork.php';
-require './test/double/functions.php';
+require './vendor/autoload.php';
 
 require './src/Abc/Example.php';
